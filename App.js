7c7742e7f2421b76662a91e67f2b75dd2bd63cef
AliciaL22 <aliciaxulin@gmail.com>
2021-03-27 12:57:38 -0700

    added timer

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import * as React from 'react';
-import { Text, View, Pressable, Image, StyleSheet } from 'react-native';
+import { Text, View, Pressable, Image, StyleSheet, Dimensions } from 'react-native';
 import Constants from 'expo-constants';
  
+let screenWidth = Dimensions.get('window').width;
+let screenHeight = Dimensions.get('window').height;
+
 
 export default class App extends React.Component {
 
   constructor() {
     super();
+    
+  }
+
+  //UPDATE WINDOW
+  updateWindow = () => {
+    screenWidth = Dimensions.get('window').width;
+    screenHeight = Dimensions.get('window').height;
+
+    this.setState({ something: true });
+  };
+
+  //TIMER FOR WINDOW DIMENSION CHECK
+  componentDidMount = () => {
+    this.counter = setInterval(() => {
+      this.updateWindow();
+    }, 500);
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Pressable style={styles.playButton} >
-          <Text style={styles.playText}>▶️</Text>
+          <Text style={styles.playText}>{screenWidth}</Text>
         </Pressable>
       </View>
     );
@@ -27,7 +46,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   playText: {
-    fontSize: 96,
+    fontSize: screenWidth/2,
     textAlign: 'center',
   },
 });
