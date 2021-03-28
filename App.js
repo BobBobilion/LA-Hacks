a2447751e048fb5dee6f3151b97f65aff2485268
AliciaL22 <aliciaxulin@gmail.com>
2021-03-27 17:46:15 -0700

    Update App.js

--- App.js
+++ App.js
@@ -32,68 +32,67 @@ export default class App extends React.Component {
     });
 
     this.state = {
-      assignments,
+      assignments,practiceSet: {
+        mon:[{
+          meetingLink: "link",
+          className: "Arthur Facredyn",
+          startTime: "9",
+          endTime: "12",
+        },
+        {
+          meetingLink: "link2",
+          className: "Arthur Facredyn2",
+          startTime: "9",
+          endTime: "12",
+        }],
+  
+        tues:[{
+          meetingLink: "link3",
+          className: "Arthur Facredyn3",
+          startTime: "12",
+          endTime: "13",
+        }],
+  
+        wed:[{
+          meetingLink: "link4",
+          className: "Arthur Facredyn4",
+          startTime: "8",
+          endTime: "10",
+        }],
+  
+        thurs:[{
+          meetingLink: "link5",
+          className: "Arthur Facredyn5",
+          startTime: "9",
+          endTime: "12",
+        },
+        {
+          meetingLink: "link6",
+          className: "Arthur Facredyn6",
+          startTime: "7",
+          endTime: "9",
+        }],
+  
+        fri:[{
+          meetingLink: "link7",
+          className: "Arthur Facredyn7",
+          startTime: "14",
+          endTime: "16",
+        },]},
+
+
+        openClass: 0,
+        showGoing: 0,
+        day: '',
+        nameClass: '',
+        link: '',
+        timeStart: '',
+        timeEnd: '',
+
     }
     
   }
 
-  state = {
-    openClass: 0,
-    showGoing: 0,
-    day: '',
-    nameClass: '',
-    link: '',
-    timeStart: '',
-    timeEnd: '',
-
-    practiceSet: [
-      {mon:[{
-        meetingLink: "link",
-        className: "Arthur Facredyn",
-        startTime: "9",
-        endTime: "12",
-      },
-      {
-        meetingLink: "link2",
-        className: "Arthur Facredyn2",
-        startTime: "9",
-        endTime: "12",
-      }]},
-
-      {tues:[{
-        meetingLink: "link3",
-        className: "Arthur Facredyn3",
-        startTime: "12",
-        endTime: "13",
-      }]},
-
-      {wed:[{
-        meetingLink: "link4",
-        className: "Arthur Facredyn4",
-        startTime: "8",
-        endTime: "10",
-      }]},
-
-      {thurs:[{
-        meetingLink: "link5",
-        className: "Arthur Facredyn5",
-        startTime: "9",
-        endTime: "12",
-      },
-      {
-        meetingLink: "link6",
-        className: "Arthur Facredyn6",
-        startTime: "7",
-        endTime: "9",
-      }]},
-
-      {fri:[{
-        meetingLink: "link7",
-        className: "Arthur Facredyn7",
-        startTime: "14",
-        endTime: "16",
-      },]}],
-  }
 
 
   //UPDATE WINDOW
@@ -110,11 +109,11 @@ export default class App extends React.Component {
     this.setDate();
   }
 
-  grabMeetings = () => {
-    return this.state.practiceSet.map(element, index) => {
+  // grabMeetings = () => {
+  //   return this.state.practiceSet.map(element, index) => {
       
-    }
-  }
+  //   }
+  // }
 
   //MAKES A TABLE FOR ASSIGNMENTS
   listAssignments = () => {
@@ -205,7 +204,7 @@ export default class App extends React.Component {
 
         
         <NormalText>
-          {this.state.day}
+          {this.state.practiceSet.mon[0].meetingLink}
         </NormalText>
 
         <View style={styles.sectionBox}>
